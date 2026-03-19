7d8ecb32914612b28506c8aad62da16198680358
Amin_ Ihsan <iamin01@bora.sciclone.wm.edu>
2026-03-19 05:21:43 -0400

    tweaked readme

--- assignments/assignment_06/scripts/02_flye_2.9.6_conda_install.sh
+++ assignments/assignment_06/scripts/02_flye_2.9.6_conda_install.sh
@@ -6,7 +6,7 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda env remove -y -n flye-env
 mamba create -y -n flye-env -c bioconda -c conda-forge flye=2.9.6
 conda activate flye-env
-flye --version
+flye -v
 conda env export --no-builds > ./flye-env.yml
 
 conda deactivate
